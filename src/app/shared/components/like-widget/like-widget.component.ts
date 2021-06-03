251b04606e587d4db7acdd50e62656b1f4551a1c
Henrique La Montagna <henriquelamontagna@gmail.com>
2021-06-03 18:13:06 -0300

    Creating like-widget component and your module

---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: [ './like-widget.component.scss' ]
+})
+export class LikeWidgetComponent {
+
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes: number = 0;
+  @Input() public id = null;
+
+}
