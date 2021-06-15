484b22898b85c893fcf79564b3eb3ac34d2e914a
Henrique La Montagna <henriquelamontagna@gmail.com>
2021-06-14 22:51:02 -0300

    spyOn test

--- src/app/shared/components/like-widget/like-widget.components.spec.ts
+++ src/app/shared/components/like-widget/like-widget.components.spec.ts
@@ -1,17 +1,61 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { UniqueIdService } from "../../service/unique-id/unique-id.service";
 import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
 
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
-
-  beforeEach(() => {
-    component = new LikeWidgetComponent(new UniqueIdService());
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        LikeWidgetComponent
+      ],
+      imports: [
+        FontAwesomeModule
+      ],
+      providers: [
+        UniqueIdService
+      ]
+    }).compileComponents;
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
-  it('', () => {
+  it('Should Create Component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  });
 
+  it('Should auto generate ID when id input property is missing', () => {
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT generate ID when id input property is present', () => {
+    const component = fixture.componentInstance;
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  });
+
+  it(`${LikeWidgetComponent.prototype.like.name} Should test Output Property When Emit a Event`, () => {
+    fixture.detectChanges();
+    const component = fixture.componentInstance;
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+    });
+    component.like();
+  });
+
+  it(`${LikeWidgetComponent.prototype.like.name} Should test Output Property When Emit a Event V2`, () => {
+    fixture.detectChanges();
+    const component = fixture.componentInstance;
+    spyOn(component.liked, 'emit');
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 
 });
